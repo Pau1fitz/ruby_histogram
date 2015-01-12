dbdea0d4db70dc848fc0162736f6f7ddb3295a98
Paul Fitzgerald <paulfitz99@gmail.com>
2015-01-12 20:54:05 +0000

    Create histogram.rb

---
+++ histogram.rb
@@ -0,0 +1,11 @@
+puts "Enter your text here please"
+text = gets.chomp
+words = text.split(" ")
+frequencies =Hash.new(0)
+
+words.each { |word| frequencies[word] +=1 }
+frequencies = frequencies.sort_by { |a, b| b}
+frequencies.reverse!
+frequencies.each do |a,b|
+    puts a + " " + b.to_s
+end
